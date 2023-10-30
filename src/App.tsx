f7e28b0d55e4754be07f984583d785a1458f959d
Danilo Flores <danilo040117@gmail.com>
2023-10-30 06:40:44 -0400

    fixed tokenization

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
+
+
+import Home from './pages/home';
+import Header from './components/header';
+import SideBar from './components/sidebar';
+import Empty from './pages/empty';
+
+import './assets/styles.scss'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <SideBar/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/staking" element={<Empty/>} />
+          <Route path="/coinflip" element={<Empty/>}/>
+          <Route path="/dice" element={<Empty/>}/>
+          <Route path="/tower" element={<Empty/>}/>
+          <Route path="/roulette" element={<Empty/>}/>
+          <Route path="/about" element={<Empty/>} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
